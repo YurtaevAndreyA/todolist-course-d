ce5a2646d32d5b29ea15912b7381056529a6e255
YurtaevAndreyA <YurtaevAndreyA@gmail.com>
2023-06-30 20:08:10 +0300

    start project .gitignore:   * added folders /.idea, /info
    index.tsx:
      * info about the start

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Итак, стартую!
+// Я выбрал папку в которой будет находиться мой проект (ii) и открыл в ней Git
+// Bash Here.
+// Прописал: 'yarn create react-app todolist-course-d --template typescript'.
+// И примерно через 50 сек мне пожелали: 'Happy hacking'  :)
+
+// Далее, я открыл папку с проектом (todolist-course-d) в Webstorm.
+// Создал папку info с файлами. В них я буду записывать информацию, чтобы
+// повторять ее и со временем усвоить или запомнить.
+// Эту папку, а также папку .idea, прописал в .gitignore
